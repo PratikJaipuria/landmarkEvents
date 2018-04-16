214e47ba6cfa6974f19917f78701afda425369b6
rohitdr786 <rohitdr786@gmail.com>
2018-04-16 04:23:51 -0400

    User Service

--- LandmarkEvents/src/main/java/edu/northeastern/cs5200/service/UserService.java
+++ LandmarkEvents/src/main/java/edu/northeastern/cs5200/service/UserService.java
@@ -1,8 +1,17 @@
 package edu.northeastern.cs5200.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import edu.northeastern.cs5200.models.User;
 import edu.northeastern.cs5200.repositories.UserRepository;
 
 @RestController
@@ -11,5 +20,37 @@ public class UserService {
 	@Autowired
 	UserRepository userRepository;
 	
+	@GetMapping("/api/user")
+	public List<User> findAllUsers(
+			@RequestParam(name="username", required=false)
+				String username,
+			@RequestParam(name="password", required=false)
+				String password) {
+		if(username != null && password != null) {
+			return (List<User>) userRepository.findUserByCredentials(username, password);
+		}
+		else if(username != null) {
+			return (List<User>) userRepository.findUserByUsername(username);
+		}
+		return (List<User>) userRepository.findAll();
+	}
+	
+	@GetMapping("/api/user/{userId}")
+	public User findUserById(
+			@PathVariable("userId") int uid) {
+		return userRepository.findOne(uid);
+	}
+	
+	@DeleteMapping("/api/user/{userId}")
+	public void deleteUser(@PathVariable("userId") int id) {
+		userRepository.delete(id);
+		
+	}
+	
+	@PostMapping("/api/user")
+	public User createUser(
+			@RequestBody User user) {
+		return userRepository.save(user);
+	}
 	
 }
